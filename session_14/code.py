b520457484a197a033a60d7269473e31f9064f8f
gamthub12 <thien.tx216243@sis.hust.edu.vn>
2025-05-22 20:59:21 +0700

    ok2

--- session_14/code.py
+++ session_14/code.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 X1, label1 = make_blobs(n_samples=200, n_features=2, centers=2, cluster_std = 5, random_state=123)
 plt.scatter(X1[:,0],X1[:,1], c= label1, alpha=0.7)
 plt.title('Dataset #1 : Original')
-plt.show()
 plt.savefig('session_14/dataset1.png')
+plt.show()
