80de479f55f403b9804a16455ff9bb25fe45b29e
gamthub12 <thien.tx216243@sis.hust.edu.vn>
2025-07-02 22:23:34 +0700

    update_chapter8

--- session_14/code.py
+++
@@ -1,8 +0,0 @@
-from sklearn.datasets import make_blobs, make_moons
-import matplotlib.pyplot as plt
-
-X1, label1 = make_blobs(n_samples=200, n_features=2, centers=2, cluster_std = 5, random_state=123)
-plt.scatter(X1[:,0],X1[:,1], c= label1, alpha=0.7)
-plt.title('Dataset #1 : Original')
-plt.savefig('session_14/dataset1.png')
-plt.show()
